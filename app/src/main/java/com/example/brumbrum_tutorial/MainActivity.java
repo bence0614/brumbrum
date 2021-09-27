cb3ce1708b589e402e50e59f3b3f173ad67f32ee
efef15 <kocsisbence00@gmail.com>
2021-09-27 18:21:36 +0200

    Game Loop

--- app/src/main/java/com/example/brumbrum_tutorial/MainActivity.java
+++ app/src/main/java/com/example/brumbrum_tutorial/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.brumbrum_tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * MainActivity is the entry point to our project
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set window to fullscreen(will hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game, so that objects in the Game class can be rendered to the screen
+        setContentView(new Game(this));
     }
 }
